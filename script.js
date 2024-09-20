e95a659af5a8d746215a231bd5633f6d200d01a6
Pyothrush <127397545+Pyothrush@users.noreply.github.com>
2024-09-20 22:09:01 +0200

    Wrote function for playing 5 rounds

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getComputerChoice(){
     let rngmove = moves[Math.floor(Math.random() * moves.length)];
     return rngmove;
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 function getHumanChoice(){
     let playermove = prompt("Choose your move");
@@ -11,14 +11,31 @@ function getHumanChoice(){
     
 }
 //console.log(getHumanChoice());
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = getHumanChoice().toLowerCase();
-    computerChoice = getComputerChoice();
-    if(humanChoice.localeCompare(computerChoice) == 0) {
-        console.log("it's a draw!");
+
+
+//playRound();
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+        humanChoice = getHumanChoice().toLowerCase();
+        computerChoice = getComputerChoice();
+        if(humanChoice.localeCompare(computerChoice) == 0) {
+            console.log("it's a draw!");
+        }else if(humanChoice == "paper" && computerChoice == "rock" || humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper"){
+            console.log("You win!");
+            humanScore++;
+        }else {
+            console.log("You loose");
+            computerScore++;
+        }
+        console.log("Your score: "+humanScore+" Computer's score: "+computerScore);
+    }
+    
+    for(i=0; i<=5; i++){
+        playRound();
     }
 }
-playRound();
+playGame();
