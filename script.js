de2eee4ab69d5fce3320bccb72414e1fa3a0bb62
Pyothrush <127397545+Pyothrush@users.noreply.github.com>
2024-09-20 20:49:34 +0200

    Doesn't work.

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-const moves = ["rock", "paper", "scissors"];
-let compChosen;
 function getComputerChoice(){
+    const moves = ["rock", "paper", "scissors"];
     let rng = Math.floor(Math.random() * moves.length);;
-    let compChosen = moves[rng];
-    console.log(compChosen);
+    moves[rng];
+    
 }
-getComputerChoice();
+
+function getHumanChoice(){
+    prompt("Choose your move!");
+}
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+        humanChoice = getComputerChoice();
+        computerChoice = getComputerChoice();
+        if (humanChoice === computerChoice){
+            console.log("It's a draw!");
+        }
+        else if(humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "rock" && computerChoice == "scissors")
+        {
+            console.log("You win!");
+            humanScore++;
+        }
+        else{
+            console.log("You loose!");
+            computerScore++;
+        } 
+
+    } 
+       
+    for(let i=0; i<5; i++){
+        playRound();
+    }
+}
+playGame();
