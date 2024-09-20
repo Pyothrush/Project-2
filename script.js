c1d4e879ae48cac42e570012d6a0857cac26b5cf
Pyothrush <127397545+Pyothrush@users.noreply.github.com>
2024-09-20 21:53:16 +0200

    Added function for comparing strings

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ function getComputerChoice(){
     let rngmove = moves[Math.floor(Math.random() * moves.length)];
     return rngmove;
 }
-//console.log(getComputerChoice());
+console.log(getComputerChoice());
 
 function getHumanChoice(){
-    let playermove = prompt("Choose your move").toLowerCase();
+    let playermove = prompt("Choose your move");
     return playermove;
     
 }
 //console.log(getHumanChoice());
 let humanScore = 0;
 let computerScore = 0;
+
 function playRound(humanChoice, computerChoice){
-    
+    humanChoice = getHumanChoice().toLowerCase();
+    computerChoice = getComputerChoice();
+    if(humanChoice.localeCompare(computerChoice) == 0) {
+        console.log("it's a draw!");
+    }
 }
+playRound();
