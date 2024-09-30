e3260c2eb00637224637002d88696963f298c56a
Pyothrush <127397545+Pyothrush@users.noreply.github.com>
2024-09-30 22:17:13 +0200

    Added simple UI and made the game run after clicking buttons

--- script.js
+++ script.js
@@ -3,39 +3,50 @@ function getComputerChoice(){
     let rngmove = moves[Math.floor(Math.random() * moves.length)];
     return rngmove;
 }
-//console.log(getComputerChoice());
 
-function getHumanChoice(){
-    let playermove = prompt("Choose your move");
-    return playermove;
+function getHumanChoice(element){
+    return element.id;
+}
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(humanScore < 5 && computerScore < 5){
+        if((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+        ){
+            humanScore++;
+            winners.textContent = "You win!";
+        }else if (humanChoice === computerChoice){
+            winners.textContent = "Its a tie";
+        }else{
+            computerScore++;
+            winners.textContent = "Computer wins!";
+        }
+        score.textContent = "Your score: " + humanScore + " Computer score: " + computerScore;
+    }else{
+        if(humanScore>computerScore){
+            winners.textContent += " You won the game!";
+        }else{winners.textContent += " You lost the game";}
+    }
     
 }
+const container = document.querySelector("#container");
+const score = document.createElement("div");
+
+const winners = document.createElement("div");
+container.appendChild(score);
+container.appendChild(winners);
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const humanChoice = getHumanChoice(e.target);
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
+});
 //console.log(getHumanChoice());
 
 
-
-//playRound();
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice){
-        humanChoice = getHumanChoice().toLowerCase();
-        computerChoice = getComputerChoice();
-        if(humanChoice.localeCompare(computerChoice) == 0) {
-            console.log("it's a draw!");
-        }else if(humanChoice == "paper" && computerChoice == "rock" || humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper"){
-            console.log("You win!");
-            humanScore++;
-        }else {
-            console.log("You loose");
-            computerScore++;
-        }
-        console.log("Your score: "+humanScore+" Computer's score: "+computerScore);
-    }
-    
-    for(i=0; i<=4; i++){
-        playRound();
-    }
-}
-playGame();
